c5e516e412bf056f2ddd3cc08673743ba9cec272
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2021-03-23 16:03:19 +0300

    adds authentication

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -7,6 +7,7 @@ defmodule RumblWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug RumblWeb.Auth
   end
 
   pipeline :api do
@@ -17,6 +18,8 @@ defmodule RumblWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
